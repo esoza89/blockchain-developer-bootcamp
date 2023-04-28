44e6379edd022c248f32bb2709c268026ce4d008
Efrain Soza <efrain.sc89@outlook.com>
2023-04-27 20:15:29 -0500

    5. Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,12 +13,39 @@ contract Token {
 	mapping (address => uint256) public balanceOf;
 	//Send tokens
 
+	event Transfer(
+		address indexed from,
+		address indexed to,
+		uint256 value
+	);
 
-	constructor (string memory _name, string memory _symbol, uint256 _total) {
+	constructor (string memory _name,
+		string memory _symbol,
+		uint256 _total
+	) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _total* (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
 
+	function transfer(address _to, uint256 _value)
+		public
+		returns (bool success)
+	{
+		//require sender has enough tokens to spend
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
+
+		//deduct tokens from sender
+		balanceOf[msg.sender] = balanceOf[msg.sender]
+			- _value;
+
+		//credit tokens to receiver
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		//Emit Event
+		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
 }
--- test/Token.js
+++ test/Token.js
@@ -10,14 +10,19 @@ describe ('Token', () => {
 //Test go inside here...
 	let token,
 		accounts,
-		deployer
+		deployer,
+		receiver
 
 	beforeEach(async () => {
 		//fetch token from Blockchain
-		const Token = await ethers.getContractFactory('Token')
-		token = await Token.deploy('Efris Coin','EFRIS', '1000000') //deploys test js BC
+		const Token = await ethers.getContractFactory
+			('Token')
+		token = await Token.deploy('Efris Coin',
+			'EFRIS',
+			 '1000000') //deploys test js BC
 		accounts = await ethers.getSigners()
 		deployer = accounts[0] //fectching accounts and deployer
+		receiver = accounts[1]
 	}) 
 
 	describe('Deployment', () => {
@@ -37,16 +42,82 @@ describe ('Token', () => {
 		})
 
 		it('has correct decimals', async () => {
-			expect(await token.decimals()).to.equal(decimals)
+			expect(await token.decimals()).to.equal
+				(decimals)
 		})
 
 		it('has correct supply', async () => {
 			//const supply = tokens('1000000')//turning ether units to wei
-			expect(await token.totalSupply()).to.equal(totalSupply)
+			expect(await token.totalSupply()).to.equal
+				(totalSupply)
 		})
 
 		it('assigns total supply to deployer', async () => {
-			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+			expect(await token.balanceOf(deployer.address))
+				.to.equal(totalSupply)
+		})
+
+	})
+
+	describe('Sending tokens', () => {
+		let amount
+		let totalSupply
+		let remanent
+		let transaction
+		let result
+
+		describe('Success', () => {
+			beforeEach(async () => {
+				amount = 100
+				totalSupply = 1000000
+				remanent = totalSupply - amount
+				amount = tokens(amount)
+				totalSupply = tokens(totalSupply)
+				remanent = tokens(remanent)
+
+				transaction = await token.connect(deployer)
+					.transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+
+			it('transfers token balances', async () =>
+			{
+				//Ensure tokens were transfered
+				expect(await token.balanceOf(deployer.address))
+					.to.equal(remanent)
+				expect(await token.balanceOf(receiver.address))
+					.to.equal(amount)
+			})
+
+			it('emits a transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('Transfer')
+
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects insuficient balances',
+				async () => {
+				//transfer more tokens that deployer has
+				const invalidAmount = tokens(100000000)
+				await expect(token.connect(deployer)
+					.transfer(receiver.address,
+					invalidAmount)).to.be.reverted
+			})
+
+			it('rejects wrong receiver',
+				async () => {
+				//transfer more tokens that deployer has
+				const amount = tokens(100)
+				await expect(token.connect(deployer)
+					.transfer('0x0000000000000000000000000000000000000000',
+					amount)).to.be.reverted
+			})
 		})
 	})
 })
