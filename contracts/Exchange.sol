0d7ea308c792977ffaf80b2183a2fbbb7c202bdb
Efrain Soza <efrain.sc89@outlook.com>
2023-05-04 14:22:39 -0500

    9. Deposit tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -2,14 +2,45 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "./Token.sol";
 
 contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
+	mapping(address => mapping(address => uint256)) public tokens;
+	//Token, user, amount
+	event Deposit(address token, address user, uint256 amount,
+		uint256 balance);
 
-	constructor( address _feeAccount, uint256 _feePercent)
+	constructor(address _feeAccount, uint256 _feePercent)
 	{
 		feeAccount = _feeAccount;
 		feePercent = _feePercent;
+	}
+
+	//Deposit token
+	function depositToken(address _token, uint256 _amount)
+		public {
+
+		//transfer token to exchange
+		require(Token(_token).transferFrom(msg.sender,
+		address(this), _amount));
+
+		//update user balances
+		tokens[_token][msg.sender] = tokens[_token][msg.sender]
+			+ _amount;
+
+		//Emit an event
+		emit Deposit(_token, msg.sender, _amount,
+			tokens[_token][msg.sender]);
+	}
+
+	//Check balances
+	function balanceOf(address _token, address _user)
+		public
+		view
+		returns (uint256)
+	{
+		return tokens[_token][_user];
 	}
 }
