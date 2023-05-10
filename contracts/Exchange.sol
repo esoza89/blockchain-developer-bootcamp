937ce249321fbf11c307adb9179e7352c171bc2d
Efrain Soza <efrain.sc89@outlook.com>
2023-05-10 18:34:21 -0500

    13. Fill orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,6 +11,7 @@ contract Exchange {
 	mapping(uint256 => _Order) public orders;
 	uint256 public orderCount;
 	mapping(uint256 => bool) public orderCancelled;
+	mapping(uint256 => bool) public orderFilled;
 
 	//Token, user, amount
 	event Deposit(address token, address user, uint256 amount,
@@ -36,6 +37,17 @@ contract Exchange {
 	uint256	amountGet,
 	address tokenGive,
 	uint256 amountGive,
+	uint256 timestamp
+	);
+
+	event Trade (
+	uint256 id,
+	address user,
+	address tokenGet,
+	uint256	amountGet,
+	address tokenGive,
+	uint256 amountGive,
+	address creator,
 	uint256 timestamp
 	);
 
@@ -106,7 +118,7 @@ contract Exchange {
 	) public {
 		require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
 
-		++orderCount;
+		orderCount ++;
 		orders[orderCount] = _Order(
 			orderCount,
 			msg.sender,
@@ -143,6 +155,74 @@ contract Exchange {
 			_order.amountGet,
 			_order.tokenGive,
 			_order.amountGive,
+			block.timestamp
+		);
+	}
+
+	function fillOrder(uint256 _id) public {
+		require(_id > 0 && _id <= orderCount);
+		require(!orderFilled[_id]);
+		require(!orderCancelled[_id]);//requires them to be false
+
+
+		//fetch order
+		_Order storage _order = orders[_id];
+
+		//execute the trade
+		_trade(
+			_order.id,
+			_order.user,
+			_order.tokenGet,
+			_order.amountGet,
+			_order.tokenGive,
+			_order.amountGive
+		);
+
+		//update filled order register
+		orderFilled[_order.id] = true;
+	}
+
+	function _trade(
+		uint256 _OrderId,
+		address _user,
+		address _tokenGet,
+		uint256 _amountGet,
+		address _tokenGive,
+		uint256 _amountGive
+		) internal {
+		
+		/* fee is paid by the user who fills the order
+		(msg.sender), fee is deducted from _amount get */
+
+		uint256 _feeAmount = (_amountGet * feePercent) / 100;
+
+		//Trading pairs
+		tokens[_tokenGet][msg.sender] =
+			tokens[_tokenGet][msg.sender] -
+				(_amountGet + _feeAmount);
+		//msg.sender is the one filling the order
+		//order user is the one who creted the order
+		tokens[_tokenGet][_user] = 
+			tokens[_tokenGet][_user] + _amountGet;
+
+		//charge fees
+		tokens[_tokenGet][feeAccount] =
+			tokens[_tokenGet][feeAccount] + _feeAmount;
+
+		tokens[_tokenGive][_user] =
+			tokens[_tokenGive][_user] - _amountGive;
+
+		tokens[_tokenGive][msg.sender] =
+			tokens[_tokenGive][msg.sender] + _amountGive;		 
+	
+		emit Trade(
+			_OrderId,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			_user,
 			block.timestamp
 		);
 	}
